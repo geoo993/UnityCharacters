323210f0ca7dd3d6b0e886b7eb2a7dac9f9cb6a0
GEORGE QUENTIN <ggeoo93@gmail.com>
2016-05-15 16:57:04 +0100

    completed gradient skybox

--- CharacterObjects/Assets/Scripts/GradientTexture.cs
+++ CharacterObjects/Assets/Scripts/GradientTexture.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO; // Used for writing PNG textures to disk
 
 public static class GradientToTexture{
 	
@@ -86,10 +85,11 @@ public class GradientTexture : MonoBehaviour {
 
 			renderer.material.mainTexture = null;
 
-			cubemap = new Cubemap(resolution, TextureFormat.ARGB32, false);
+			//cubemap = new Cubemap(resolution, TextureFormat.ARGB32, false);
+			//texture = new Texture2D (cubemap.width, cubemap.height, TextureFormat.RGB24, false);
 
-			texture = new Texture2D (cubemap.width, cubemap.height, TextureFormat.RGB24, false);
-			//texture = new Texture2D (resolution, resolution, TextureFormat.RGB24, false);
+
+			texture = new Texture2D (resolution, resolution, TextureFormat.RGB24, false);
 			texture.name = "gradientTexture";
 
 			texture.wrapMode = TextureWrapMode.Clamp;
@@ -102,158 +102,21 @@ public class GradientTexture : MonoBehaviour {
 			//renderer.material.mainTexture = texture;
 
 
-			CubemapFace[] faces = new CubemapFace[] {
-				CubemapFace.PositiveX, CubemapFace.NegativeX,
-				CubemapFace.PositiveY, CubemapFace.NegativeY,
-				CubemapFace.PositiveZ, CubemapFace.NegativeZ };
-
-			foreach (CubemapFace face in faces) {
-				texture.SetPixels(cubemap.GetPixels(face));        
-				cubemap.Apply ();
-				File.WriteAllBytes(Application.dataPath + "/" + cubemap.name + "_" + face.ToString() + ".png", texture.EncodeToPNG());
-			}   
-
-
+//
 			//Material mat = new Material (Shader.Find ("Skybox/Cubemap"));
 			Material mat = new Material (Shader.Find (".ShaderExample/ScreenScapeGradient"));
 			//cubemap = Resources.Load ("ClearSkyRadiance") as Cubemap;
 			mat.SetTexture("_MainTex",texture);
 
+
 			renderer.material = mat;
-			cubemap.Apply ();
+			//cubemap.Apply ();
 
 		
 		}
 
-
-
-
-
 	}
 		
-
-
-	// This is the coroutine that creates the cubemap images
-	void CreateCubeMap()
-	{
-		// Initialise a new cubemap
-		Cubemap cm = new Cubemap(resolution, TextureFormat.RGB24, true);
-
-		// Disable any renderers attached to this object which may get in the way of our camera
-		if(GetComponent<Renderer>()) {
-			GetComponent<Renderer>().enabled = false;
-		}
-
-		// Face render order: Top, Right, Front, Bottom, Left, Back
-		Quaternion[] rotations = { Quaternion.Euler(-90,0,0), Quaternion.Euler(0,90,0), Quaternion.Euler(0,0,0), Quaternion.Euler(90,0,0), Quaternion.Euler(0,-90,0), Quaternion.Euler(0,180,0)};
-		CubemapFace[] faces = { CubemapFace.PositiveY, CubemapFace.PositiveX, CubemapFace.PositiveZ, CubemapFace.NegativeY, CubemapFace.NegativeX, CubemapFace.NegativeZ };
-
-		// Create a single face matching the settings of the cubemap itself
-		Texture2D face = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
-
-		face.name = "gradientTexture";
-		// Use this to prevent white borders around edge of texture
-		face.wrapMode = TextureWrapMode.Clamp;
-
-		face.filterMode = FilterMode.Trilinear;//FilterMode.Bilinear; //FilterMode.Point;
-		face.anisoLevel = 9;
-
-		addGradient (gradientColor);
-		face = gradientColor.ToTexture (resolution);
-
-		//renderer.material.mainTexture = texture;
-
-
-		//		// Loop through and create each face
-		for(int i = 0; i < 6; i++) {
-			
-		
-			face = Resize(face, resolution, resolution);
-			// Flip the image across the x axis
-			face = Flip(face);
-			// Retrieve the pixelarray of colours for the current face
-			Color[] faceColours = face.GetPixels();
-			// Set the current cubemap face
-			cm.SetPixels(faceColours, faces[i], 0);
-			// Save the texture
-			SaveTextureToFile(face, gameObject.name + "_" + faces[i].ToString() + ".png");
-		}
-		// Apply the SetPixel changes to the cubemap faces to make them take effect     
-		cm.Apply();
-
-		//		// Assign the cubemap to the _Cube texture of this object's material
-			if(GetComponent<Renderer>().material.HasProperty("_Cube")) {
-				GetComponent<Renderer>().material.SetTexture("_Cube", cm);
-			}
-	
-			// Cleanup
-			DestroyImmediate(face);
-			//DestroyImmediate(cam);
-	
-			// Re-enable the renderer
-			if(renderer) {
-				GetComponent<Renderer>().enabled = true;
-			}
-}
-
-
-	private Texture2D CaptureScreen() {
-		Texture2D result;
-		Rect captureZone = new Rect( 0f, 0f, Screen.width, Screen.height );
-		result = new Texture2D( Mathf.RoundToInt(captureZone.width), Mathf.RoundToInt(captureZone.height), TextureFormat.RGB24, false);
-		result.ReadPixels(captureZone, 0, 0, false);
-		result.Apply();
-		return result;
-	}
-
-	// Save a Texture2D as a PNG file
-	// http://answers.unity3d.com/questions/245600/saving-a-png-image-to-hdd-in-standalone-build.html
-	private void SaveTextureToFile(Texture2D texture, string fileName) {
-		byte[] bytes = texture.EncodeToPNG();
-		FileStream file = File.Open(Application.dataPath + "/" + fileName,FileMode.Create);
-		BinaryWriter binary = new BinaryWriter(file);
-		binary.Write(bytes);
-		file.Close();
-	}
-
-	// Resize a Texture2D
-	// http://docs-jp.unity3d.com/Documentation/ScriptReference/Texture2D.GetPixelBilinear.html
-	Texture2D Resize(Texture2D sourceTex, int Width, int Height) {
-		Texture2D destTex = new Texture2D(Width, Height, sourceTex.format, true);
-		Color[] destPix = new Color[Width * Height];
-		int y = 0;
-		while (y < Height) {
-			int x = 0;
-			while (x < Width) {
-				float xFrac = x * 1.0F / (Width );
-				float yFrac = y * 1.0F / (Height);
-				destPix[y * Width + x] = sourceTex.GetPixelBilinear(xFrac, yFrac);
-				x++;
-			}
-			y++;
-		}
-		destTex.SetPixels(destPix);
-		destTex.Apply();
-		return destTex;
-	}
-
-	// Flip/Mirror the pixels in a Texture2D about the x axis
-	Texture2D Flip(Texture2D sourceTex) {
-		// Create a new Texture2D the same dimensions and format as the input
-		Texture2D Output = new Texture2D(sourceTex.width, sourceTex.height, sourceTex.format, true);
-		// Loop through pixels
-		for (int y = 0; y < sourceTex.height; y++)
-		{
-			for (int x = 0; x < sourceTex.width; x++)
-			{
-				// Retrieve pixels in source from left-to-right, bottom-to-top
-				Color pix = sourceTex.GetPixel(sourceTex.width + x, (sourceTex.height-1) - y);
-				// Write to output from left-to-right, top-to-bottom
-				Output.SetPixel(x, y, pix);
-			}
-		}
-		return Output;
-	}
 
 
 
---
+++ CharacterObjects/Assets/Scripts/Skybox.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skybox : MonoBehaviour {
+
+	Camera camera;
+
+	public static Material CreateSkyboxMaterial(SkyboxManifest manifest)
+	{
+		Material result = new Material(Shader.Find("RenderFX/Skybox"));
+		result.SetTexture("_FrontTex", manifest.textures[0]);
+		result.SetTexture("_BackTex", manifest.textures[1]);
+		result.SetTexture("_LeftTex", manifest.textures[2]);
+		result.SetTexture("_RightTex", manifest.textures[3]);
+		result.SetTexture("_UpTex", manifest.textures[4]);
+		result.SetTexture("_DownTex", manifest.textures[5]);
+		return result;
+	}
+	public static Material CreateGradientMaterial(Color topColor, Color middleColor, Color bottomColor)
+	{
+		Material result = new Material (Shader.Find (".ShaderExample/GradientThreeColor"));
+
+		result.SetFloat ("_Middle", 0.6f);
+		result.SetColor("_ColorTop", topColor);
+		result.SetColor("_ColorMid",middleColor);
+		result.SetColor("_ColorBot", bottomColor);
+		return result;
+	}
+
+	public Texture2D[] textures;
+
+	private int resolution = 256;
+	private float duration = 10.0f;
+	private Color CurrentColor =  Color.red;
+	private Color previousColor = Color.blue;
+	private float t = 0;
+
+	private Gradient addGradient ()
+	{
+		Gradient g = new Gradient ();
+
+		GradientColorKey blue = new GradientColorKey(Color.blue, 0.0f);
+		GradientColorKey white = new GradientColorKey(Color.white, 0.5f);
+		GradientColorKey yellow = new GradientColorKey(Color.yellow, 1f);
+		GradientAlphaKey blueAlpha = new GradientAlphaKey(1,0);
+		GradientAlphaKey yellowAlpha = new GradientAlphaKey(1,1);
+
+
+		GradientColorKey[] colorKeys = new GradientColorKey[]{blue, white, yellow};
+		GradientAlphaKey[] alphaKeys = new GradientAlphaKey[]{blueAlpha,yellowAlpha};
+
+		g.SetKeys(colorKeys, alphaKeys);
+
+		return g;
+	}
+
+	private Texture2D CreateGradientTexture(){
+
+
+		Texture2D texture = new Texture2D (resolution, resolution, TextureFormat.RGB24, false);
+		texture.name = "gradientTexture";
+
+		texture.wrapMode = TextureWrapMode.Clamp;
+		Gradient gradientColor = addGradient ();
+
+		texture = gradientColor.ToTexture (resolution);
+		texture.Apply ();
+
+		return texture;
+	}
+
+
+	//void OnEnable()
+	//void OnEnableUpdate()
+	void Update()
+	{
+		
+		////type 1  set with external texture
+//		SkyboxManifest manifest = new SkyboxManifest(textures[0], textures[1], textures[2], textures[3], textures[4], textures[5]);
+
+
+		/////type 2  set with procedurally generated texture
+//		Texture2D text = CreateGradientTexture();
+//		SkyboxManifest manifest = new SkyboxManifest(text, text, text, text, text, text);
+//		Material material = CreateSkyboxMaterial(manifest);
+//
+
+
+		////type 3 set with external material
+		if (t < 1.0f) {
+			t += Time.deltaTime * (1.0f / duration);
+		} else {
+			t = 0;
+			duration = Random.Range (20f, 50f);
+
+			CurrentColor = previousColor;
+			previousColor = ExtensionMethods.RandomColor ();
+		}
+		Color one = Color.Lerp (CurrentColor,previousColor, t);
+
+		print("time: "+t+" duration:  "+ duration);
+
+		Material material = CreateGradientMaterial(Color.black,Color.black, one);
+		SetSkybox(material);
+		//enabled = false;
+
+	}
+
+	void SetSkybox(Material material)
+	{
+		GameObject camera = Camera.main.gameObject;
+		Skybox skybox = camera.GetComponent<Skybox>();
+		if (skybox == null)
+			skybox = camera.AddComponent<Skybox>();
+		RenderSettings.skybox = material;
+	}
+}
+
+public struct SkyboxManifest
+{
+	public Texture2D[] textures;
+
+	public SkyboxManifest(Texture2D front, Texture2D back, Texture2D left, Texture2D right, Texture2D up, Texture2D down)
+	{
+		textures = new Texture2D[6]
+		{
+			front,
+			back,
+			left,
+			right,
+			up,
+			down
+		};
+	}
+}
